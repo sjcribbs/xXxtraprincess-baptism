7f745b3c67c5663225d6ee304c61bbf347e7611b
sjcribbs <scribb@saic.edu>
2018-11-27 13:26:13 -0600

    new main.js file && testing bookmarklets

--- xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
+++ xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
@@ -1,24 +1,3 @@
-const socket = io()
-const enter = document.querySelector('#enter')
-const utxt = document.querySelector('#usertext')
-const uname = document.querySelector('#username')
-
-
-function addNewTextBox(name,txt){
-    let div = document.createElement('div')
-    div.innerHTML = `<b>${name}</b>: ${txt}`
-    document.body.appendChild(div)
-}
-
-enter.addEventListener('click',function(){
-    addNewTextBox( uname.value, utxt.value )
-
-    socket.emit('enter-click',{
-        name:uname.value,
-        text:utxt.value
-    })
-})
-
-socket.on('new-msg',function(data){
-    addNewTextBox(data.name,data.text)
-})
+(function (){
+  alert("hello")
+}) ();
