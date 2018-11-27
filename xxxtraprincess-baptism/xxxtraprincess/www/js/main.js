2be93c3cdbbf3b4f7f4eeef9a496879a9a74c652
sjcribbs <scribb@saic.edu>
2018-11-27 14:36:34 -0600

    text replacement test

--- xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
+++ xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
@@ -1,3 +1,7 @@
 (function (){
-  alert("hello")
+  console.log("bookmarklet starting");
+  let paragraphs = document.getElementsByTagName('p');
+  for (let i = 0; i < paragraphs.length; i++ ) {
+    paragraphs[i].innerHTML = 'kitten';
+  }
 }) ();
