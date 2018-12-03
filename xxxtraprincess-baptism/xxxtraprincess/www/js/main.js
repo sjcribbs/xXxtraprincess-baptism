0d0788d94c7187a169159aa55b0b4007223a804a
sjcribbs <scribb@saic.edu>
2018-12-03 16:08:06 -0600

    test code

--- xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
+++ xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
@@ -1,4 +1,7 @@
 (function (){
-  alert('hello');
+  console.log("bookmarklet starting");
+  let paragraphs = document.getElementsByTagName for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].innerHTML='kitten';
+  }
 }) ();
 //only pops up on the first load
