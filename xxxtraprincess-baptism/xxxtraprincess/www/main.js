774f45e79e640d739232d618b0978d7e0021c692
sjcribbs <scribb@saic.edu>
2018-12-04 16:59:42 -0600

    added comments on script.src and paragraphs[i] web console alert

--- xxxtraprincess-baptism/xxxtraprincess/www/main.js
+++ xxxtraprincess-baptism/xxxtraprincess/www/main.js
@@ -5,6 +5,7 @@
     paragraphs[i].innerHTML = 'kitten';
   }
 }) ();
+//script.src = url + "?" + new Date().getTime();
 
 // loading failed with socket
-//
+// still getting message that 'paragraphs[i]' is not defined
