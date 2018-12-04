ba03b8cb4100eaa37dc48ff34401fa228e9dfc94
sjcribbs <scribb@saic.edu>
2018-12-03 18:42:25 -0600

    testing bookmarklet

--- xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
+++ xxxtraprincess-baptism/xxxtraprincess/www/js/main.js
@@ -1,7 +1,10 @@
 (function (){
   console.log("bookmarklet starting");
-  let paragraphs = document.getElementsByTagName for (let i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].innerHTML='kitten';
+  let paragraphs = document.getElementsByTagName
+  for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].innerHTML = 'kitten';
   }
 }) ();
-//only pops up on the first load
+
+// loading failed with socket
+//
