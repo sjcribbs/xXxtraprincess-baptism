7c4a4bc8fa46aca10a807b27aea64a96926bc0ea
sjcribbs <scribb@saic.edu>
2018-12-10 16:47:46 -0600

    comment explaination for js redirect

--- xxxtraprincess/www/main.js
+++ xxxtraprincess/www/main.js
@@ -1,3 +1,4 @@
 window.location.href = 'http://xxxtrabaptism.me'
 // redirect to a new page
 window.location.replace('http://xxxtrabaptism.me')
+// replace the original document in browser history with new one
