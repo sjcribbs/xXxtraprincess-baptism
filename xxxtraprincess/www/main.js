d9e8b199724db2913c7c053734910066f1e90325
sjcribbs <scribb@saic.edu>
2018-12-10 16:49:20 -0600

    comment credit (person and page) for code

--- xxxtraprincess/www/main.js
+++ xxxtraprincess/www/main.js
@@ -2,3 +2,4 @@ window.location.href = 'http://xxxtrabaptism.me'
 // redirect to a new page
 window.location.replace('http://xxxtrabaptism.me')
 // replace the original document in browser history with new one
+// script by: Daniyal Hamid [https://www.designcise.com/web/tutorial/how-to-redirect-to-another-web-page-using-javascript]
