f3515b0e6dc9480ffdfc953c48c9c64efe4c6996
sjcribbs <scribb@saic.edu>
2018-12-11 15:50:17 -0600

    conditional statement main.js

--- xxxtraprincess/www/main.js
+++ xxxtraprincess/www/main.js
@@ -1,5 +1,7 @@
+if (window.location !== "http.//xxxtrabaptism.me/"){
 window.location.href = 'http://xxxtrabaptism.me'
 // redirect to a new page
 window.location.replace('http://xxxtrabaptism.me')
 // replace the original document in browser history with new one
 // script by: Daniyal Hamid [https://www.designcise.com/web/tutorial/how-to-redirect-to-another-web-page-using-javascript]
+}
